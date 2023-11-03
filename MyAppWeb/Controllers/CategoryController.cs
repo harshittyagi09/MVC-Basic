60c08a94a294c59c992909f610b37af2f26dd4d3
harshittyagi09 <tyagiharshit85@gmail.com>
2023-11-03 16:59:24 +0530

    Edit | Delete (Get,Post Method) | Client Side Validations

--- MyAppWeb/Controllers/CategoryController.cs
+++ MyAppWeb/Controllers/CategoryController.cs
@@ -18,10 +18,80 @@ namespace MyAppWeb.Controllers
             IEnumerable<Category> categories = _context.Categories;
             return View(categories);
         }
+
+        // For Creation of Category
         [HttpGet]
         public IActionResult Create()
         {
            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // To Edit the Particular Category
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id== null || id==0) {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        // To Delete the particular Category 
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategory(int? id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
